a6e8b62637a87b30856c2077e6279fd1452c6334
imran5644 <imi56444@gmail.com>
2021-04-07 05:30:46 +0800

    Add the createTodolist Function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import _ from 'lodash';
 function component() {
     const element = document.createElement('div');
   
+    init();
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   
@@ -10,3 +11,28 @@ function component() {
   }
   
   document.body.appendChild(component());
+
+  const createTodoList = () => {
+
+    const row = document.createElement('div');
+    row.classList.add('row');
+
+    const col = document.createElement('div');
+    col.classList.add('col');
+
+
+    const todoTask = document.createElement('p');
+    todoTask.classList.add('todoTask');
+    todoTask.textContent="hello";
+  
+    row.appendChild(col);
+    row.appendChild(todoTask);
+  
+    return row;
+  }
+
+
+  const init = () => {
+    let container = document.getElementById('container');
+    container.appendChild(createTodoList());
+  }
