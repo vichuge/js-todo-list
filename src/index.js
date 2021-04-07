d1cbeaf5dc8ce798a5fde747910e890a5c8b134d
V <victor.hugo.pacheco.flores@gmail.com>
2021-04-07 15:33:02 -0500

    List the tasks dinamically with js (not finished)

--- src/index.js
+++ src/index.js
@@ -42,21 +42,18 @@ const createTodoList = () => {
   todoTask.classList.add('todoTask');
 
   todoTask.innerHTML += tasks.map((task) => {
-    return `
+    /*return `
     <li class="list-group-item ${task.priority === 'low' ? 'bgreen' : task.priority === 'medium' ? 'byellow' : task.priority === 'high' ? 'bred' : ''}">
       <div class="row">
         <div class="col-12 d-flex justify-content-between">
           <input
             class="form-check-input me-1"
             type="checkbox"
-            value=""
-            aria-label="..."
           />
           <h5 class="mb-1">${task.title}</h5>
           <p>${task.desc}</p>
           <small>${task.date}</small>
           <input
-            
             class="btn btn-primary"
             type="button"
             value="Delete"
@@ -64,7 +61,50 @@ const createTodoList = () => {
           />
         </div>
       </div>
-    </li>`;
+    </li>`;*/
+    const li = document.createElement('li');
+    li.classList.add('list-group-item');
+    task.priority === 'low' ? li.classList.add('bgreen') : task.priority === 'medium' ? li.classList.add('byellow') : task.priority === 'high' ? li.classList.add('bred') : '';
+
+    const div1 = document.createElement('div');
+    div1.classList.add('row');
+
+    const div2 = document.createElement('div');
+    div2.classList.add('col-12');
+    div2.classList.add('d-flex');
+    div2.classList.add('justify-content-between');
+
+    const input1 = document.createElement('input');
+    input1.classList.add('form-check-input');
+    input1.classList.add('me-1');
+    input1.type ='checkbox';
+
+    const h5 = document.createElement('h5');
+    h5.classList.add('mb-1');
+    h5.innerHTML = task.title;
+
+    const p = document.createElement('p');
+    p.innerHTML = task.desc;
+
+    const small = document.createElement('small');
+    small.innerHTML = task.date;
+
+    const input2 =document.createElement('input');
+    input2.classList.add('btn');
+    input2.classList.add('btn-primary');
+    input2.type ='button';
+    input2.id = 'deleteid';
+
+    div2.appendChild(input1);
+    div2.appendChild(h5);
+    div2.appendChild(p);
+    div2.appendChild(small);
+    div2.appendChild(input2);
+    div1.appendChild(div2);
+    li.appendChild(div1);
+    console.log(li);
+    return li;
+    //return `<li>hi there! ${task.title}</li>`;
   }).join('');
 
   //const del = document.getElementById('deleteid');
@@ -116,8 +156,8 @@ const init = () => {
   let create = document.getElementById('create');
   create.addEventListener('click', () => { addTask(); });
 
-  let deleteid = document.getElementById('deleteid');
-  deleteid.addEventListener('click', () => {deleteTask(id); });
+  //let deleteid = document.getElementById('deleteid');
+  //deleteid.addEventListener('click', () => {deleteTask(id); });
 }
 
 init();
