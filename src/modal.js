d25194533caf13f0f01b308aaec9a71e3facc6b6
imran5644 <imi56444@gmail.com>
2021-04-09 04:25:11 +0800

    Correct the class create syntax

--- src/modal.js
+++ src/modal.js
@@ -52,25 +52,22 @@ const funcs = {
         closeModal2();
     },
     buildModal() {
-        const div1 = document.createElement('div');
-        div1.setAttribute('id', 'container');
-        div1.classList.add="list";
-        div1.classList.add="mb-4";
-
+        
         const div2 = document.createElement('div');
         div2.setAttribute('id', 'modal');
-        div2.classList.add="container";
+        div2.classList.add('container');
+        
         
         const div3 = document.createElement('div');
-        div3.classList.add="row";
-        div3.classList.add="mb-3";
+        div3.classList.add("row");
+        div3.classList.add("mb-3");
 
         const div4 = document.createElement('div');
-        div4.classList.add="col-12";
+        div4.classList.add('col-12');
 
         const button1 = document.createElement('button');
-        button1.classList.add="btn";
-        button1.classList.add="btn-primary";
+        button1.classList.add('btn');
+        button1.classList.add('btn-primary');
         button1.setAttribute("data-bs-toggle","modal");
         button1.setAttribute("data-bs-target","modal");
         button1.innerHTML = 'Add a new task';
@@ -85,30 +82,30 @@ const funcs = {
 
         const div5 = document.createElement('div');
         div5.setAttribute('id', 'exampleModal');
-        div5.classList.add="modal";
-        div5.classList.add="fade";
+        div5.classList.add('modal');
+        div5.classList.add('fade');
         div5.setAttribute("tabindex","-1");
         div5.setAttribute("aria-labelledby","exampleModalLabel");
         div5.setAttribute("aria-hidden","true");
 
         const div6 = document.createElement('div');
-        div6.classList.add="modal-dialog";
+        div6.classList.add('modal-dialog');
         
         const div7 = document.createElement('div');
-        div7.classList.add="modal-content";
+        div7.classList.add('modal-content');
         
         const div8 = document.createElement('div');
-        div8.classList.add="modal-header";
+        div8.classList.add('modal-header');
           
         const h5 = document.createElement('h5');
         h5.setAttribute('id', 'exampleModalLabel');
-        h5.classList.add="modal-title";  
+        h5.classList.add('modal-title');  
         h5.innerHTML="Enter the task information" 
 
             
         const button2 = document.createElement('button');
         button2.type="button";
-        button2.classList.add="btn-close";
+        button2.classList.add('btn-close');
         button2.setAttribute("data-bs-dismiss","modal");
         button2.setAttribute("aria-label","Close");
         button2.setAttribute("id","close");
@@ -123,16 +120,16 @@ const funcs = {
 
         
         const div9 = document.createElement('div');
-        div9.classList.add="modal-body";
+        div9.classList.add("modal-body");
         
         const div10 = document.createElement('div');
-        div10.classList.add="row";
+        div10.classList.add("row");
         
         const div11 = document.createElement('div');
-        div11.classList.add="col-12";
+        div11.classList.add("col-12");
 
         const p1 = document.createElement('p');
-        p1.classList.add="error";
+        p1.classList.add("error");
         p1.setAttribute("id","error");
 
         div11.appendChild(p1);
@@ -141,8 +138,8 @@ const funcs = {
     
         
         const div12 = document.createElement('div');
-        div12.classList.add="row";
-        div12.classList.add="mb-2";   
+        div12.classList.add("row");
+        div12.classList.add("mb-2");   
         
 
         const label1 = document.createElement('label');
@@ -161,18 +158,18 @@ const funcs = {
         
 
         const div13 = document.createElement('div');
-        div13.classList.add="row";
+        div13.classList.add("row");
 
         const div14 = document.createElement('div');
-        div14.classList.add="col-12";
-        div14.classList.add="mb-2";  
+        div14.classList.add("col-12");
+        div14.classList.add("mb-2");  
 
         const label2 = document.createElement('label');
         label2.htmlFor = 'description';
         label2.innerHTML="Description";
 
         const textarea1 = document.createElement('textarea');
-        textarea1.classList.add="form-control";
+        textarea1.classList.add("form-control");
         textarea1.setAttribute("placeholder","Add description here");
         textarea1.name="description";
             
@@ -185,10 +182,10 @@ const funcs = {
 
 
         const div15 = document.createElement('div');
-        div15.classList.add="row";
+        div15.classList.add("row");
 
         const div16 = document.createElement('div');
-        div16.classList.add="col-12"; 
+        div16.classList.add("col-12"); 
 
         const label3 = document.createElement('label');
         label3.htmlFor = 'dueDate';
@@ -205,11 +202,11 @@ const funcs = {
            
         
         const div17 = document.createElement('div');
-        div17.classList.add="row";
+        div17.classList.add("row");
 
         const div18 = document.createElement('div');
-        div18.classList.add="col-12"; 
-        div18.classList.add="mt-3"; 
+        div18.classList.add("col-12"); 
+        div18.classList.add("mt-3"); 
 
         const select = document.createElement('select');
         select.classList.add="form-select"; 
@@ -239,20 +236,20 @@ const funcs = {
         
 
         const div19 = document.createElement('div');
-        div19.classList.add="modal-footer";      
+        div19.classList.add("modal-footer");      
 
 
         const button3 = document.createElement('button');
         button3.type="button";
-        button3.classList.add="btn";
-        button3.classList.add="btn-secondary";
+        button3.classList.add("btn");
+        button3.classList.add("btn-secondary");
         button3.setAttribute("data-bs-dismiss","modal");
         button3.innerHTML="Close"
            
         const button4 = document.createElement('button');
         button4.type="button";
-        button4.classList.add="btn";
-        button4.classList.add="btn-primary";
+        button4.classList.add("btn");
+        button4.classList.add("btn-primary");
         button4.innerHTML="Add Task"
         button4.setAttribute("id","createTask");
               
@@ -261,18 +258,18 @@ const funcs = {
         div19.appendChild(button4);
     
        
-        div1.appendChild(div2);
-        div1.appendChild(div5);
-        div1.appendChild(div9);
-        div1.appendChild(div12);
-        div1.appendChild(div13);
-        div1.appendChild(div15);
-        div1.appendChild(div17);
-        div1.appendChild(div19);
+        
+        div2.appendChild(div5);
+        div2.appendChild(div9);
+        div2.appendChild(div12);
+        div2.appendChild(div13);
+        div2.appendChild(div15);
+        div2.appendChild(div17);
+        div2.appendChild(div19);
        
 
-        console.log(div1);
-        return div1;
+        console.log(div2);
+        return div2;
 
     }
 }
