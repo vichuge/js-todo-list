6c169c222516734f13f81bc7cafdcc00720e0e3c
V <victor.hugo.pacheco.flores@gmail.com>
2021-04-12 20:56:30 -0500

    Add better way to show tasks

--- src/list.js
+++ src/list.js
@@ -20,6 +20,7 @@ const lists = {
         }).join('');
     },
     createTodoList(tasks = 'default') {
+        
         let container = document.getElementById('container');
         container.innerHTML = '';
         const row = document.createElement('div');
@@ -82,7 +83,54 @@ const lists = {
         } else {
             const allProjects = allProjectsFunc();
             for (let i = 0; i < allProjects.list.length; i += 1) {
-                for (let j = 0; j < allProjects.list[i].list.length; j += 1) {
+                const mappy = allProjects.list[i].list.map((task, index) => {
+                    const li = document.createElement('li');
+                    li.classList.add('list-group-item');
+                    task.priority === 'low' ? li.classList.add('bgreen') : task.priority === 'medium' ? li.classList.add('byellow') : task.priority === 'high' ? li.classList.add('bred') : '';
+    
+                    const div1 = document.createElement('div');
+                    div1.classList.add('row');
+    
+                    const div2 = document.createElement('div');
+                    div2.classList.add('col-12');
+                    div2.classList.add('d-flex');
+                    div2.classList.add('justify-content-between');
+    
+                    const input1 = document.createElement('input');
+                    input1.classList.add('form-check-input');
+                    input1.classList.add('me-1');
+                    input1.type = 'checkbox';
+                    input1.addEventListener('click', () => { changeCheck(allProjects.list[i], task.title) });
+                    (task.check === true) ? input1.setAttribute('checked', ''): '';
+    
+                    const h5 = document.createElement('h5');
+                    h5.classList.add('mb-1');
+                    h5.innerText = task.title;
+    
+                    const p = document.createElement('p');
+                    p.innerText = task.desc;
+    
+                    const small = document.createElement('small');
+                    small.innerText = task.date;
+    
+                    const input2 = document.createElement('input');
+                    input2.classList.add('btn');
+                    input2.classList.add('btn-primary');
+                    input2.type = 'button';
+                    input2.id = 'deleteid';
+                    input2.value = 'Delete';
+                    input2.addEventListener('click', () => { deleteTask(allProjects.list[i], task.title); });
+    
+                    div2.appendChild(input1);
+                    div2.appendChild(h5);
+                    div2.appendChild(p);
+                    div2.appendChild(small);
+                    div2.appendChild(input2);
+                    div1.appendChild(div2);
+                    li.appendChild(div1);
+                    todoTask.appendChild(li);
+                }).join('');
+                /*for (let j = 0; j < allProjects.list[i].list.length; j += 1) {
                     const li = document.createElement('li');
                     li.classList.add('list-group-item');
                     allProjects.list[i].list[j].priority === 'low' ? li.classList.add('bgreen') : allProjects.list[i].list[j].priority === 'medium' ? li.classList.add('byellow') : allProjects.list[i].list[j].priority === 'high' ? li.classList.add('bred') : '';
@@ -129,7 +177,7 @@ const lists = {
                     div1.appendChild(div2);
                     li.appendChild(div1);
                     todoTask.appendChild(li);
-                }
+                }*/
             }
         }
         row.appendChild(col, tasks.list);
@@ -148,7 +196,7 @@ const lists = {
     }
 }
 
-const deleteTask = (project, titleProject, list) => {
+const deleteTask = (project, titleProject, list = 'default') => {
     for (let i=0; i < project.list.length; i += 1) {
         if (titleProject === project.list[i].title) {
             project.list.splice(i, 1);
@@ -165,4 +213,7 @@ const changeCheck = (project, titleProject) => {
     }
 }
 
+const runList = () => {
+
+}
 export default lists;
